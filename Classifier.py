fdbf43a3bee922f6c1e8ee5665675a2854166dcb
leavittntl <71446166+leavittntl@users.noreply.github.com>
2023-03-27 16:50:16 -0400

    Update Classifier.py
    Edit model

--- Classifier.py
+++ Classifier.py
@@ -2,7 +2,8 @@ import streamlit as st
 from transformers import pipeline
 
 # Load pre-trained text classification model
-text_classifier = pipeline("text-classification", model="text-classification", revision="text-classification-en-finetuned-sentiment")
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+text_classifier = pipeline("text-classification", model=model_name)
 
 # Create a text input box for the user to enter feedback
 feedback = st.text_input('Enter your feedback:', value='The product was great, but the delivery was delayed.')
@@ -15,3 +16,4 @@ st.write('Classification results:')
 for i, result in enumerate(results):
     NPSPlusVal = (f"Feedback {i+1}: Category: {result['label']}, Score: {result['score']}")
     st.write(NPSPlusVal)
+
