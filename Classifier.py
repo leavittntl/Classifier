5820ecd8fff60fa80c1cc056b6b988957bab57e1
leavittntl <71446166+leavittntl@users.noreply.github.com>
2023-03-27 16:21:20 -0400

    Create Main

---
+++ Classifier.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load pre-trained text classification model
+text_classifier = pipeline("text-classification")
+
+# Create a text input box for the user to enter feedback
+feedback = st.text_input('Enter your feedback:', value='The product was great, but the delivery was delayed.')
+
+# Classify the feedback into different topics
+results = text_classifier(feedback)
+
+# Display the classification results
+st.write('Classification results:')
+for i, result in enumerate(results):
+    NPSPlusVal = (f"Feedback {i+1}: Category: {result['label']}, Score: {result['score']}")
+    st.write(NPSPlusVal)
