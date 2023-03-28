459b4d1f8126d39ea2f494492622a548d508e6e8
leavittntl <71446166+leavittntl@users.noreply.github.com>
2023-03-28 09:13:59 -0400

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -1,47 +1,23 @@
 import json
 import streamlit as st
-from streamlit.components.v1 import ComponentMixin
-from streamlit.components.v1 import declare_component
 from transformers import pipeline
 
 # Load pre-trained text classification model
 model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 text_classifier = pipeline("text-classification", model=model_name)
 
-# Define a new custom component for passing data to a JavaScript variable
-NPS_COMPONENT_ID = "nps_component"
-
-class NPSComponent(ComponentMixin):
-    def __init__(self, feedback):
-        self.feedback = feedback
-        self.results = text_classifier(feedback)
-        self.label = self.results[0]['label']
-        self.score = self.results[0]['score']
-        
-    def _render(self):
-        return json.dumps({
-            "label": self.label,
-            "score": self.score
-        })
-
-nps_component = declare_component(NPS_COMPONENT_ID, NPSComponent)
-
 # Create a text input box for the user to enter feedback
 feedback = st.text_input('Enter your feedback:', value='The product was great, but the delivery was delayed.')
 
-# Call the custom component to get the classification results
-results_json = nps_component(feedback)
+# Classify the feedback into different topics
+results = text_classifier(feedback)
 
-# Parse the results JSON and extract the label and score
-results = json.loads(results_json)
-label = results['label']
-score = results['score']
-
-# Create a string containing the feedback category and score
-NPSPlusVal = f"Feedback: Category: {label}, {score}"
+label = results[0]['label']
+score = results[0]['score']
 
 # Display the classification results
 st.write('Classification results:')
+NPSPlusVal = (f"Feedback: Category: {label}, {score}")
 st.write(results)
 st.write(NPSPlusVal)
 
